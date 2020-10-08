1deddc1f76ff903987c3935ebae09e02b9f9e836
Matthew Messinger <mattmess1221@gmail.com>
2020-10-08 18:43:40 -0400

    Updates

---
+++ precache-manifest.5e7ea3691129f01d0198c60e0d0bcdd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a107276d01392d6f9e4342aab62e2f8d",
+    "url": "/minelpweb/index.html"
+  },
+  {
+    "revision": "d6c551b52c8c1214bf0a",
+    "url": "/minelpweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d29dc4baa94d19eb049",
+    "url": "/minelpweb/static/js/2.5924f443.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minelpweb/static/js/2.5924f443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c551b52c8c1214bf0a",
+    "url": "/minelpweb/static/js/main.2844a368.chunk.js"
+  },
+  {
+    "revision": "455c3e6d394d4883c0a8",
+    "url": "/minelpweb/static/js/runtime-main.31c3d941.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minelpweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
